65329398f8781da1a8cd44fe9b0aebd94fdd74c0
eunjeeing <ejjeongggg@gmail.com>
2024-09-11 11:46:28 +0900

    :sparkles: Feat: 공통 컬러 설정

---
+++ src/styles/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+    white: '#FFFFFFFF',
+    main: '#3E8D60',
+    light_green: '#F0F6F3',
+    mid_green: '#479C53',
+    dark_green: '#2F674B',
+    black: '#141414',
+    light_gray: '#F7F7F7',
+    gray: '#9B9E9C',
+    nav_gray: '#D4D6D4',
+};
